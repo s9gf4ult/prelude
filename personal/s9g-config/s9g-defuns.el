da4e80d2e9d3d9bc030219288cc9d3063c42fe4c
Aleksey Uimanov <s9gf4ult@gmail.com>
2017-02-22 15:07:16 +0500

    add snippet and function

--- personal/s9g-config/s9g-defuns.el
+++ personal/s9g-config/s9g-defuns.el
@@ -25,6 +25,26 @@
 
 ;;; Code:
 
+
+(defun uniq-lines (beg end)
+  "Unique lines in region.
+Called from a program, there are two arguments:
+BEG and END (region to sort)."
+  (interactive "r")
+  (save-excursion
+    (save-restriction
+      (narrow-to-region beg end)
+      (goto-char (point-min))
+      (while (not (eobp))
+        (kill-line 1)
+        (yank)
+        (let ((next-line (point)))
+          (while
+              (re-search-forward
+               (format "^%s" (regexp-quote (car kill-ring))) nil t)
+            (replace-match "" nil nil))
+          (goto-char next-line))))))
+
 (defun key-binding-at-point (key)
   (mapcar (lambda (keymap) (when (keymapp keymap)
                              (lookup-key keymap key)))
